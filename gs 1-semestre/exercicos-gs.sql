3c1ad74ba81ef4e40dc520ba1924c847f2a98a55
Lara Gonçalves da Silva <laragonsilva26122003@gmail.com>
2024-05-28 02:04:44 +0000

    exc 2

--- gs 1-semestre/exercicos-gs.sql
+++ gs 1-semestre/exercicos-gs.sql
@@ -1,2 +1,6 @@
 ''' 1) Utilizando a estrutura WITH/CTE, escreva uma consulta SQL que calcule a temperatura média em cada local com base nas informações de condições ambientais.
 Exiba o nome de cada local junto com sua respectiva temperatura média. Atenção: Soluções diferentes da instrução WITH serão consideradas incorretas ''' 
+
+''' 
+Utilizando as tabelas localizacoes, condicoes_ambientais e poluicoes, escreva uma consulta para exibir o nome da localização, a data das condições ambientais, o tipo de poluente e a média da temperatura, ordenando os resultados pelos nomes das localizações, datas das condições ambientais e tipos de poluentes das localizações 'Baía de Guanabara' e 'Recife de Tubbataha', para os poluentes 'Sedimentos' e 'Mercúrio'.
+'''
